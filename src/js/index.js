7835fc75b928c023b324cde9bcdcd47d5088ece1
ElizabethSh <liza.shoshkina@gmail.com>
2021-06-04 19:18:58 +0200

    Adapt tabs for the mobile version, accordion

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@ import "../scss/styles.scss";
 
 const header = document.querySelector(`.header`);
 const logo = document.querySelector(`.header-logo svg`);
+const tabSection = document.querySelector(`.tabs`);
+const activeTab = tabSection.querySelector(`.tab-header-active`);
+const tabList = tabSection.querySelector(`.tab-header-wrap`);
 
 window.addEventListener(`scroll`, () => {
     if (window.pageYOffset > 1) {
@@ -11,4 +14,13 @@ window.addEventListener(`scroll`, () => {
         header.classList.remove('header-fixed');
         logo.style.height = '120px';
     }
-})
+});
+
+activeTab.addEventListener(`click`, () => {
+    tabSection.classList.toggle(`closed`);
+});
+
+tabList.addEventListener(`click`, (evt) => {
+    activeTab.textContent = evt.target.textContent;
+    tabSection.classList.add(`closed`);
+});
